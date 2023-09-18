bbe2da7e818828f57e73b1ac05e6b00b3571f42d
Asanm20120122214085 <144373094+Asanm20120122214085@users.noreply.github.com>
2023-09-18 19:27:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-class Player:
-  def play(Self):
-    print("The player is playing cricket.")
-  class Batsman(Player):
-    def play(self):
-      print("The batsman is batting.")
-  class Bowler(Player):
-    def play(self):
-      print("The bowler is bowling .")
-batsman= Batsman()
-bowler= Bowler()
-
-
-batsman.play()
-bowler.play()
-      
+class Bank_Account:
+  def __init__(self):
+    Self.balance =0
+    print("Hello!!!Welcome to the deposit & withdrawl machine")
+  def deposit(self):
+    amount=float(input("Enter amount to be Deposited:"))
+    self.balance += amount
+    print("\n Amount   Deposited:",amount)
+  def withdraw(self):
+    amount=float(input("Enter amount to be withdrawan:"))
+if self.balance>=amount:
+    self.balance-=amount
+      print("\n you withdraw:",amount)
+  else:
+      print("\n Insufficient balance")
+  def display(self):
+    print("\n Net Avialable   Balance=",self.balance)
+  S= Bank_Account()
+  S.deposit()
+  S.withdraw()
+  S.display()
+        
